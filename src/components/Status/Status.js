252f5bf2adb4ec6c6ca124ac07b37b5dced0dc37
Dawid Bienkowski <marksoft1992@gmail.com>
2019-12-03 23:54:37 +0100

    add new function in status component

--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -4,6 +4,7 @@ import Footer from '../Footer/Footer';
 import styles from '../Status/Status.scss';
 import PropTypes from 'prop-types';
 class Status extends React.Component {
+    state = { step: 0 };
     static propTypes = {
         falcon: PropTypes.number,
     lightsaber: PropTypes.number,
@@ -11,25 +12,62 @@ class Status extends React.Component {
     render() {
         const {falcon,lightsaber}= this.props.power;
         const suma = falcon+lightsaber;
-      
+        
+
+    
+      function Powerstats(){
+          if (falcon ==0){{return(
+            falcon ==0 && <div className={styles.cards}>
+            <img className={styles.imgstatusrevers} src='src/img/milenium.png' ></img>
+              <h3 className={styles.title}>{falcon} Kw</h3>
+                      </div>);
+              }}
+            return (<div className={styles.cards}>
+                {
+          falcon !=0 && <div className={styles.cards}>
+          <img className={styles.imgstatus} src='src/img/milenium.png' ></img>
+            <h3 className={styles.title}>{falcon} Kw</h3> 
+
+            </div>
+            
+            }</div>
+                    );
+                    }
+                 
+                    function PowerStatsLightSaber(){
+                        if (lightsaber ==0){{return(
+                          lightsaber ==0 && <div className={styles.cards}>
+                          <img className={styles.imgstatusrevers} src='src/img/light.png' ></img>
+                            <h3 className={styles.title}>{lightsaber} Kw</h3>
+                                    </div>);
+                            }}
+                          return (<div className={styles.cards}>
+                              {
+                        lightsaber !=0 && <div className={styles.cards}>
+                        <img className={styles.imgstatus} src='src/img/light.png' ></img>
+                          <h3 className={styles.title}>{lightsaber} Kw</h3> 
+              
+                          </div>
+                          
+                          }</div>
+                                  );
+                                  }
+         
+       
         return (
   <section >    
+      
         <h2 className={styles.title}>Total Power :{suma}</h2>
             <div className={styles.component}>
-                <div className={styles.cards}>
-                    <img className={styles.imgstatus} src='src/img/light.png'></img>
-        <h3 className={styles.title}>{lightsaber}Kw</h3>    
-                </div>
-                <div className={styles.cards}>
-                    <img className={styles.imgstatus} src='src/img/milenium.png'></img>
-        <h3 className={styles.title}>{falcon} Kw</h3>
-                </div>
+               < PowerStatsLightSaber lightsaber/>
+                <Powerstats falcon />
+             
             </div>
     <Footer/>
     </section>
         );
  
-
+        
         }
 }
 export default Status;
